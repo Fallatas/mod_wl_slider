5662e0132c1f260bef40ece4acc19493920bca84
Fallatas <fallatas85@winterling-labs.com>
2018-10-10 14:35:10 +0200

    commits corrected

--- control.php
+++ control.php
@@ -3,7 +3,6 @@
 * @package     mod_wl-slider
 * @author      Thomas Winterling
 * @email       info@winterling-labs.com
-* @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
 * @license     GNU General Public License version 2 or later; see LICENSE.txt
 */
 defined('_JEXEC') or die;
